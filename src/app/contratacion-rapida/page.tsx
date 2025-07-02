b7e4da6939c93b4ff8eff61268e41a1a2f5aea7d
JorgeAlexis <joromeromartinez@deloittemx.com>
2025-07-02 15:19:57 -0600

    feat se agrego el responsivo a los elementos y se agrego una clase css para el radiant de 4 colores - contratacion rapida

--- src/app/contratacion-rapida/page.tsx
+++ src/app/contratacion-rapida/page.tsx
@@ -12,24 +12,28 @@ export default async function ContratacionRapida(){
   const cards = await getCards(ids);
 
   return (
-      <div className="border border-red-500 items-center justify-items-center
+      <div className=" items-center justify-items-center
       box-content box-border 
-      lg:w-280 md:w-200 sm:w-[402px] 
-      lg:h-80 md:h-70 sm:h-[622px]">
+      lg:w-280 md:w-200 xsm:w-[320px] 
+      lg:h-80 md:h-80 xsm:h-[550px]">
          
-        <h1 className="font-bold md:text-[25px] lg:mb-3 lg:mt-3 md:mb-1">hazlo fácil, hazlo izzi</h1>
+        <h1 className="font-bold md:text-[25px] xsm:text-[25px] lg:mb-3 lg:mt-3 md:mb-1 xsm:mt-7">hazlo fácil, hazlo izzi</h1>
   
-        <div className="border border-green-500 md:flex md:flex-row sm:flex-col">
+        <div className=" relative  md:flex md:flex-row sm:flex-col ">
+           
+           {/* barra radiante_> top-[px] left-[] maneja la posicion...md:mx-[] top-[] bottom-[] manejan el ancho/altura de la barra*/}
+           <div className="hidden md:block absolute top-[115px] left-0 right-0 h-[1px] z-0 md:mx-22 gradient-bar-horizontal " />
+           <div className="block md:hidden absolute left-[28.5%] top-10 bottom-10 w-[1px] z-0 gradient-bar-vertical " />
+
           {cards.map((card: any) => {
             const bodyText = card.fields;
             const imgURL = bodyText?.image?.fields?.image?.fields?.file?.url;
             const dotURL =  card.fields?.dot?.fields?.file?.url;
   
-            return (  // border border-red-500   
-              <div key={card.sys.id} className="border border-blue-500 box-border md:w-40 md:h-50 md:p-1 md:mx-3">
-                <div className="md:items-center md:justify-items-center sm:items-baseline sm:justify-items-start">
-                    <div className="border border-yellow-500 box-border w-20 h-20 
-                    flex items-center justify-center ">
+            return (  // border border-red-500
+              <div key={card.sys.id} className="relative z-10 box-border md:w-40 xsm:w-80 md:h-50 md:p-1 md:mx-3 flex md:flex-col sm:flex-row xsm:my-6">
+                <div className=" items-center md:flex sm:flex xsm:flex md:flex-col sm:flex-row xsm:flex-row">
+                    <div className=" box-border w-20 h-20 flex items-center justify-center ">
                         {imgURL && (
                             <Image
                             alt={'Images'}
@@ -39,7 +43,7 @@ export default async function ContratacionRapida(){
                             /> 
                         )}
                     </div>
-                    <div className="border border-yellow-500 md:mt-1 md:mb-6">
+                    <div className=" md:mt-1 md:mb-6 md:ml-0 xsm:ml-4">
                         {dotURL && (
                             <Image
                             alt={'Images'}
@@ -50,7 +54,8 @@ export default async function ContratacionRapida(){
                         )}
                     </div>
                 </div>
-                <div className="text-[12px] text-center  align-middle flex items-center justify-center ">
+                <div className="text-[12px] md:text-center xsm:text-left align-middle flex items-center justify-center 
+                md:pl-0 xsm:pl-7">
                   <p>{bodyText.entryBody}</p>      
                 </div>
               </div>
@@ -63,8 +68,4 @@ export default async function ContratacionRapida(){
 }
 
 
-/* 
-<h1 className="font-bold lg:text-[36px] md:text-[25px] md:mb-1">hazlo fácil, hazlo izzi</h1>
-<div className="border border-red-500 items-center justify-items-center box-content box-border lg:w-280 md:w-200 sm:w-40 lg:h-100 md:h-70"> 
-<div className="lg:text-[18px] md:text-[12px] text-center  align-middle flex items-center justify-center ">
-*/
+
