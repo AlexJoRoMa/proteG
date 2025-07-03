363b95151c853ec84993f5a53c430b5749f3dd46
MACF94 <migue9048@gmail.com>
2025-07-02 21:29:49 -0500

    [feat] Se crea las flechas para pasar las imagenes en el carrusel - home-module-tv

---
+++ src/components/molecules/CarouselArrowsComponent.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image"
+
+type CarouselArrowsComponentProps = {
+    scrollPrev: () => void
+    scrollNext: () => void
+    classPrevButton?: string
+    classNextButton?: string
+}
+
+const CarouselArrowsComponent = ({scrollPrev, scrollNext, classPrevButton, classNextButton}:CarouselArrowsComponentProps) => {
+  return (
+    <>
+      <button title="Previous" aria-label="Previous" type="button" className={`embla__prev ${classPrevButton}`} onClick={scrollPrev}>
+        &nbsp;
+        <Image
+          src="/FlechaIzquierda.webp"
+          alt="Prev"
+          width={32}
+          height={32}/>
+      </button>
+      <button title="Next" aria-label="Next" type="button" className={`embla__next ${classNextButton}`} onClick={scrollNext}>
+        <Image
+          src="/FlechaDerecha.webp"
+          alt="Next"
+          width={32}
+          height={32}/>
+      </button>
+    </>
+  )
+}
+
+export default CarouselArrowsComponent
