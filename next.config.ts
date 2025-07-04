5f362f72afeda14a72e2b52e80f52288a00ac4f2
JorgeAlexis <joromeromartinez@deloittemx.com>
2025-07-04 14:55:14 -0600

    fix/quitados los cambios en los archivos env, nextconfig y packagejson

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ['images.ctfassets.net'],
-  },
-  
 };
 
 export default nextConfig;
