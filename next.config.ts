5f33988baf1c9d4f228f2fd807c5c1b15768fe0c
MACF94 <migue9048@gmail.com>
2025-07-04 21:46:01 -0500

    Revert "[feat] se adiciona modo estricto de react y filtro de imagenes por host - (home-module-tv)"
    This reverts commit 8b504e49eeb14eaf0a5cb38220091202772d0802.

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-        port: "",
-        pathname: "/wikipedia/commons/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.ctfassets.net",
-        port: "",
-        pathname: "/**",
-      }
-    ],
-  }
 };
 
 export default nextConfig;
