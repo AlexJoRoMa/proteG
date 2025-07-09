85193f5211254f928d3ee66e900ad6cb11bcc269
juanarodriguez-deloitte <juanarodriguez@deloitte.com>
2025-07-09 17:33:44 -0500

    [Feat]: maquetación tarjeta internet & uso microcopy configurador

---
+++ src/types/ConfiguradorTypes.ts
@@ -0,0 +1,13 @@
+
+// props
+
+export type configuradorCopyFields = {
+    cobertura: string;
+    internet: string;
+    movil: string;
+    title: string;
+    tv: {
+        title: string;
+        description: string;
+    }
+}
