1c5914a3d67b819e06dbb0c6a1004c5d884e07c8
JorgeAlexis <joromeromartinez@deloittemx.com>
2025-07-11 12:58:55 -0600

    update-version stable antes de volver paginas dinamicas

--- src/lib/contentful/dynamic-map.ts
+++ src/lib/contentful/dynamic-map.ts
@@ -1,8 +1,10 @@
 import ChannelPromoBannerComponent from "@/components/organisms/ChannelPromoBannerComponent";
 import TabsTile from "@/components/organisms/tabs/tabsTile";
+import ContratacionRapida from '@/components/organisms/contratacion-rapida';
 
 
 export const componentMap = {
-  carouselChannel: ChannelPromoBannerComponent,
+  // carouselChannel: ChannelPromoBannerComponent,
   //tabs: TabsTile
+  cardsContratacionRapida: ContratacionRapida
 };
