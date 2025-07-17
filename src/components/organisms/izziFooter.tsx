49adf1c7b3689773f1727aeb6fb5c1ed48688a7b
diolvera <diolvera@deloittemx.com>
2025-07-17 09:42:08 -0600

    Feature: Se implementa el template con el contenido de footer

---
+++ src/components/organisms/izziFooter.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import Link from 'next/link';
+import { Accordion, AccordionItem } from '@heroui/react';
+import { IzziFooter, FooterComponentProps, IzziFooterLinks } from "@/types/FooterTypes";
+import { HeadphoneIcon, WppIcon, DropIcon, IzziLogo, ProfecoLogo } from '@/components/atoms/FooterIcons';
+
+export default function IzziFooterContent({contactData, linksData}: FooterComponentProps) {
+
+    const contactSection = contactData?.fields as IzziFooter;
+    const links = linksData?.fields as IzziFooterLinks;
+
+    const itemClasses = {
+        title: "font-normal text-medium text-white-0",
+        content: "text-small px-2",
+        indicator: "data-[open=true]:-rotate-180",
+        heading: "border-b border-gray-250"
+    }
+    
+    const subListClasses = {
+        title: "font-normal text-medium text-white-0",
+        content: "text-small px-2",
+        indicator: "data-[open=true]:-rotate-180",
+    }  
+
+    return (
+    <>
+        <footer className="bg-black-0 w-full">
+  <div className="max-w-full px-4 py-16 sm:px-6 lg:px-8">
+    <div className="container mx-auto max-w-[80%] grid grid-cols-1 md:grid-cols-2 gap-8">
+        {contactSection?.footerContactSection?.map((link, index) => ( 
+        <div className="flex flex-col items-center justify-center gap-2" key={`${link}-${index}`}>
+          <div className="text-center">
+            <h2 className="text-[20px] text-white-0">{link?.fields?.topCopy}</h2>
+
+            <p className="text-white-0">
+              {`${link?.fields?.bottomCopy} ${link?.fields?.contactNumber}`}
+            </p>
+          </div>
+          <div className='flex items-center gap-6'>
+            
+              <Link href='#' className='text-white-0 underline text-[18px] flex items-center gap-2'><HeadphoneIcon />centro de ayuda</Link>
+            
+            
+              <Link href='#' className='text-white-0 underline text-[18px] flex items-center gap-2'><WppIcon /> WhatsApp</Link>
+            
+          </div>
+        </div>
+        ))}
+    </div>
+    <div className="lg:flex lg:items-start lg:gap-8 mt-8 border-t border-gray-250 pt-8 text-white-0">
+        <div className="container mx-auto mt-8 grid grid-cols-1 gap-8 px-4">
+          <div className='grid grid-cols-1 md:grid-cols-3 md:gap-8'>
+          {links?.footerLinkSection.map((link, index) => (
+            <div className="col-span-2 sm:col-span-1" key={`${link}-${index}`}>
+              {link?.fields?.footerLink && link?.fields?.footerLink.length > 0 &&
+              <Accordion itemClasses={itemClasses}>
+                <AccordionItem indicator={<DropIcon />} aria-label={link.fields.internalName} title={link.fields.internalName}>
+                <ul>
+                {link?.fields.footerLink.map((accordion, index) => (
+                    <li key={`${accordion}-${index}`}>
+                        {accordion?.fields?.footerLink && accordion?.fields?.footerLink.length > 0 &&
+                                <Accordion itemClasses={subListClasses}>
+                                    <AccordionItem indicator={<DropIcon />} aria-label={accordion.fields.internalName} title={accordion.fields.internalName}>
+                                        <ul>
+                                        {accordion?.fields?.footerLink.map((subList, index) => (
+                                            <li key={`${subList}-${index}`}>
+                                                <Link href={`${subList.fields.navigationUrl}`}>{subList.fields.navigationTitle}</Link>
+                                            </li>
+                                        ))}
+                                        </ul>
+                                    </AccordionItem>
+                                </Accordion>
+                        }
+                        <Link href={`${accordion.fields.navigationUrl}`}>{accordion.fields.navigationTitle}</Link>
+                    </li>
+                ))}
+                </ul>
+                </AccordionItem>
+              </Accordion>
+                }
+                {!link?.fields?.footerLink &&
+                    <Link href={`${link.fields.navigationUrl}`}>{link.fields.navigationTitle}</Link>
+                }
+            </div>
+            ))}
+            </div>
+        </div>
+    </div>
+
+    <div className="mt-8 border-t border-gray-250 pt-8">
+      <div className="sm:flex sm:justify-between">
+        <IzziLogo />
+
+        <ul className="mt-8 flex flex-wrap justify-start gap-4 text-xs sm:mt-0 lg:justify-end">
+          <li>
+            <ProfecoLogo />
+          </li>
+
+          <li>
+            <p className='text-white-0 text-[14px]'>© 2024, izzi.mx. Todos los derechos reservados.</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+    </>
+    )
+}
