0290d2e43dba4e6f60d42730f3d3730b358dbc99
JorgeAlexis <joromeromartinez@deloittemx.com>
2025-07-21 13:29:18 -0600

    [UPDATE]: creado contenido en contentful y pagina dinamica

---
+++ src/components/organisms/con-izzi-tv.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { CoberturaID, StepTabEntryFields, StepTabEntrySkeleton } from '@/types/CoberturaTypes';
+
+const ConIzziTv = async ({id} : CoberturaID) =>{
+
+    return(
+        <h1>helllo</h1>
+    );
+ }
+
+
+export default ConIzziTv
--- src/lib/contentful/dynamic-map.ts
+++ src/lib/contentful/dynamic-map.ts
@@ -2,10 +2,12 @@ import ChannelPromoBannerComponent from "@/components/organisms/ChannelPromoBann
 import PagesTabsTile from "@/components/organisms/pagesTabsTile";
 import HomeCarouselCardComponent from "@/components/organisms/HomeCarouselCardComponent";
 import CoberturaModel from '@/components/organisms/cobertura-model';
+import ConIzziTv from '@/components/organisms/con-izzi-tv';
 
 export const componentMap = {
   carouselChannel: ChannelPromoBannerComponent,
   tabs: PagesTabsTile,
   homeCarousel: HomeCarouselCardComponent,
   cardCobertura: CoberturaModel,
+  modelIzziTV: ConIzziTv,
 };
