af4fb8793ffe00180245550595df1218a659d2b1
JorgeAlexis <joromeromartinez@deloittemx.com>
2025-07-21 16:12:12 -0600

    [UPDATE]: archivo type creado

--- src/components/organisms/con-izzi-tv.tsx
+++ src/components/organisms/con-izzi-tv.tsx
@@ -1,10 +1,26 @@
 import Image from "next/image";
-import { CoberturaID, StepTabEntryFields, StepTabEntrySkeleton } from '@/types/CoberturaTypes';
+import { ConIzziTvID, StepTabEntryFields, StepTabEntrySkeleton } from '@/types/ConIzziTypes';
+import { contentfulClient } from "@/services/contentful/client";
+import { Asset, Entry, EntrySkeletonType } from "contentful";
+import ButtonGhost from "../atoms/ButtonGhost";
 
-const ConIzziTv = async ({id} : CoberturaID) =>{
 
+const ConIzziTv = async ({id} : ConIzziTvID) =>{
+
+    const callCardsContent:Entry<EntrySkeletonType, undefined, string>[] | null = await contentfulClient.getEntries({
+            content_type: "izziTvModel",
+            'sys.id': id,
+            select: ['fields.cardsContent'],
+            include: 2,
+        }).then((entriesResponse) => {
+            return entriesResponse.items
+        })
+    
+      const getCardsContent: Array<Entry<StepTabEntrySkeleton>> | undefined = callCardsContent?.[0]?.fields.cardsContent as unknown as Array<Entry<StepTabEntrySkeleton>>;
+
+      console.log('>>>>> getCardsContent ', getCardsContent);
     return(
-        <h1>helllo</h1>
+        <h1>helllo3</h1>
     );
  }
 
---
+++ src/types/ConIzziTypes.ts
@@ -0,0 +1,26 @@
+import { Asset, Entry, EntrySkeletonType } from "contentful";
+
+export type ConIzziTvID = {
+    id: string;
+}
+
+interface MediaEntryFields {
+  image?: Asset;
+}
+
+interface MediaEntrySkeleton extends EntrySkeletonType{
+  contentTypeId: 'media';
+  fields: MediaEntryFields;
+}
+
+export interface StepTabEntryFields extends EntrySkeletonType{
+  image?: Entry<MediaEntrySkeleton>;
+  entryTitle?: string;
+  entryBody?: string;
+  desactivarComponentes?: boolean;
+}
+
+export interface StepTabEntrySkeleton extends EntrySkeletonType{
+  contentTypeId: 'izziTvModelTop';
+  fields: StepTabEntryFields;
+}
