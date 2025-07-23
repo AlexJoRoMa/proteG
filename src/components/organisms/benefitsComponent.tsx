5ae90a50738657ae2f847f71346b41df1b08d321
juanarodriguez-deloitte <juanarodriguez@deloitte.com>
2025-07-23 13:17:53 -0500

    [Feat]: Configuración redireccion boton "contratar ahora"

--- src/components/organisms/benefitsComponent.tsx
+++ src/components/organisms/benefitsComponent.tsx
@@ -17,13 +17,18 @@ export default async function BenefitsComponent({ id }: BenefitsContainerProps) 
 
     const entryTitle = benefitsEntry?.fields.title as string;
     const entryCta = benefitsEntry?.fields.ctaText as string;
+    const entryCtaUrl = benefitsEntry?.fields.ctaUrl as string;
     const entryData = benefitsEntry?.fields.cards as unknown as EntrySkeletonType<CardDataFields>;
 
     return (
         <div className="flex flex-col w-full self-center items-center bg-gray-50 text-black-0 gap-[40px] py-[64px]">
             <h1 className="font-bold text-[32px] lg:text-4xl text-wrap text-center leading-[48px]">{entryTitle}</h1>
             <BenefitsCardContent cards={entryData} />
-            <ButtonGhost text={entryCta} classStyles="bg-black-0 py-[14px] px-[16px] rounded-md w-[320px] h-auto text-white-0 border-none font-semibold leading-[24px] text-lg" />
+            <ButtonGhost 
+                text={entryCta} 
+                href={entryCtaUrl}
+                classStyles="bg-black-0 py-[14px] px-[16px] rounded-md w-[320px] h-auto text-white-0 border-none font-semibold leading-[24px] text-lg"
+            />
         </div>
     )
 
