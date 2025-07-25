cb66a555760e1174720a388d3b05c3677c22b35b
JorgeAlexis <joromeromartinez@deloittemx.com>
2025-07-25 12:03:16 -0600

    [FIX]: margenes

--- src/components/organisms/todo-uno.tsx
+++ src/components/organisms/todo-uno.tsx
@@ -19,7 +19,8 @@ const TodoEnUnoComp = async ({id} : TodoEnUnoCompID) =>{
   const getCardsContent: Array<Entry<StepTabEntrySkeleton>> | undefined = callCardsContent?.[0]?.fields.cardsContent as unknown as Array<Entry<StepTabEntrySkeleton>>;
   
   return (
-    <div className="bg-black w-full  lg:h-100 md:h-80 xsm:h-[620px]">
+    <div className=" bg-black w-full w-full lg:h-[450px] md:h-[408px] xsm:h-[720px]">
+      <div className=" h-full">
 
       {getCardsContent && getCardsContent.map((card: Entry<StepTabEntrySkeleton>) => {
         const { entryTitle, entryBody, desactivarBoton, textBoton1, linkBoton1, image }= card.fields as StepTabEntryFields;
@@ -31,20 +32,22 @@ const TodoEnUnoComp = async ({id} : TodoEnUnoCompID) =>{
           <div key={card.sys.id} className=" flex flex-col lg:flex-row md:flex-row w-full h-full text-white">
             {/*  div del contenido izq  */}
             <div className=" flex flex-col justify-center w-full lg:w-1/2 md:w-1/2  order-last lg:order-none md:order-none">
-            <div className="  lg:ml-50 lg:mr-10 md:ml-22 md:mr-10 flex flex-col lg:items-start lg:text-left md:items-start md:text-left xsm:items-center xsm:text-center">
+            <div className=" md:mx-md 2xl:mx-xl md:w-[70%] xsm:w-full
+            flex flex-col lg:items-start lg:text-left md:items-start md:text-left xsm:items-center xsm:text-center">
+              
               <div className=" lg:text-[36px] md:text-[22px] xsm:text-[32px] font-semibold 
               lg:mb-4 md:mb-4 xsm:mb-2 xsm:mt-4 ">
               <h2>{entryTitle}</h2>
               </div>
-              <div className=" lg:text-[18px] md:text-[14px] lg:mt-6 lg:mb-6 md:mt-6 md:mb-6 xsm:mt-4 xsm:mb-4">
+              <div className=" lg:text-[18px] md:text-[14px] lg:mt-6 lg:mb-6 md:mt-6 md:mb-6 md:mx-0 xsm:mt-4 xsm:mb-6 xsm:mx-8">
               <p >{entryBody}</p>
               </div>
               
             </div>
-            <div className="flex lg:justify-start lg:ml-50  md:ml-22 md:justify-start xsm:justify-center">
+            <div className="md:mx-md 2xl:mx-xl flex lg:justify-start md:justify-start xsm:justify-center">
                 {desactivarBoton === false && (
                   <ButtonGhost classStyles="border-white text-white text-[16px] leading-6 font-bold hover:!bg-white hover:!text-black w-full h-[48px] rounded-md
-                  max-w-[256px] md:max-w-[260px] sm:max-w-[320px]"
+                   md:max-w-[320px] xsm:w-[256px]"
                     text={textBoton1 as string} href={linkBoton1 as string}
                     />
                   )}
@@ -67,6 +70,7 @@ const TodoEnUnoComp = async ({id} : TodoEnUnoCompID) =>{
           </div>
         );
       })}
+      </div>
     </div>
   );
 }
