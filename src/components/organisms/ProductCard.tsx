e41ab5bdd3e84419b73fe90a5bf2d9c1564159fd
diolvera <diolvera@deloittemx.com>
2025-08-06 11:57:42 -0600

    fix: ajuste en bordes de card y espacios de copies

--- src/components/organisms/ProductCard.tsx
+++ src/components/organisms/ProductCard.tsx
@@ -13,8 +13,8 @@ export default function ProductCard({color, borderColor, icon, altIcon, cardData
     return (
         <>
         <div className="flex mt-8 mb-8">
-        <Card className='xl:w-full max-w-[402px] lg:max-w-[264px] xl:max-w-[284px] 3xl:max-w-[344px] 4xl:max-w-[384px] bg-white border border-solid' style={{borderColor: `${borderColor}`}}>
-            <CardHeader className='flex w-full h-[144px] mt-0' style={{background: `${color}`}}>
+        <Card className='xl:w-full max-w-[402px] lg:max-w-[264px] xl:max-w-[284px] 3xl:max-w-[344px] 4xl:max-w-[384px] bg-white border-1 border-solid rounded-md' style={{borderColor: `${borderColor}`}}>
+            <CardHeader className='flex w-full h-[144px] mt-0 rounded-t-md' style={{background: `${color}`}}>
                 <div className='flex items-center px-[24px]'>
                     <div className='pr-[16px]'>
                         <Image src={`https:${icon}`} width={72} height={72} alt={altIcon} loading='lazy'/>
@@ -25,25 +25,25 @@ export default function ProductCard({color, borderColor, icon, altIcon, cardData
                 </div>
             </CardHeader>
             <CardBody>
-            <div className="flex items-baseline text-gray-900 mx-[24px] mt-[32px] mb-[16px]">
+            <div className="flex items-baseline text-gray-900 mx-[12px] mt-[32px] mb-[16px]">
                 <span className="text-[24px] lg:text-[18px] xl:text-[20px] 3xl:text-[24px] font-extrabold">{card.content}</span>
             </div>
-            <div className="flex items-baseline text-gray-900 mx-[24px] mb-[16px]">
+            <div className="flex items-baseline text-gray-900 mx-[12px] mb-[16px]">
                 <span className="text-[18px] lg:text-[12px] xl:text-[14px] 3xl:text-[18px] font-normal">{card.pricePrefixCopy}</span>
                 <span className="text-[56px] lg:text-[50px] xl:text-[52px] 3xl:text-[56px] font-extrabold tracking-tight"> ${card.priceValue}</span>
                 <span className="ms-1 text-[18px] lg:text-[12px] xl:text-[14px] 3xl:text-[18px] font-normal text-gray-500">{card.priceSuffixCopy}</span>
             </div>
-            <div className="flex items-baseline text-gray-900 mx-[24px] mb-[24px]">
+            <div className="flex items-baseline text-gray-900 mx-[12px] mb-[24px]">
                 <span className="text-[18px] lg:text-[12px] xl:text-[14px] 3xl:text-[16px] font-normal">{card.advertisingCopy}</span>
             </div>
             </CardBody>
             <CardFooter>
                 <div className='flex flex-col w-full'>
-                    <Button as={Link} className="w-[90%] bg-color-trasparent h-[48px] border-[1px] border-solid rounded-md text-[18px] lg:text-[16px] xl:mx-[16px] 3xl:text-[18px] mx-[20px] mb-[16px]"
+                    <Button as={Link} className="w-[92%] bg-color-trasparent h-[48px] border-[1px] border-solid rounded-md text-[18px] lg:text-[16px] xl:mx-[12px] 3xl:text-[18px] mx-[20px] mb-[16px]"
                         href={card.secondaryButtonUrl}>
                             {card.secondaryButtonCopy}
                     </Button>
-                    <Button as={Link} className="w-[90%] bg-black-0 h-[48px] border-[2px] border-solid rounded-md text-white-0 text-[18px] xl:mx-[16px] lg:text-[16px] 3xl:text-[18px] mx-[20px] mb-[32px]"
+                    <Button as={Link} className="w-[92%] bg-black-0 h-[48px] border-[2px] border-solid rounded-md text-white-0 text-[18px] xl:mx-[12px] lg:text-[16px] 3xl:text-[18px] mx-[20px] mb-[32px]"
                         href={card.primaryButtonUrl}>
                             {card.primaryButtonCopy}
                     </Button>
