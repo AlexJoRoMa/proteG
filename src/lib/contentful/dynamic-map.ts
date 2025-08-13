86f63598cb962b262b170ed00410c7d7d885e6df
MACF94 <migue9048@gmail.com>
2025-08-12 20:52:15 -0500

    [fix] Se arregla un cambio que puede ocasionar un error mal intencionado - izzi-tv

--- src/lib/contentful/dynamic-map.ts
+++ src/lib/contentful/dynamic-map.ts
@@ -5,6 +5,7 @@ import CarouselCardComponent from "@/components/organisms/CarouselCardComponent"
 import GenericProductCard from "@/components/molecules/GenericProductComponent";
 import ContratacionRapida from '@/components/organisms/contratacion-rapida';
 import TodoEnUnoComp from '@/components/organisms/todo-uno';
+import AccesoConfigurador from '@/components/organisms/acceso-configurador';
 import CoberturaModel from '@/components/organisms/cobertura-model';
 import TVBeneficiosProductos from '@/components/organisms/tv-beneficios-productos';
 import BloqueSeparador from '@/components/organisms/bloque-separador';
@@ -17,6 +18,7 @@ export const componentMap = {
   benefits: BenefitsComponent,
   carouselCards: CarouselCardComponent,
   cardTodoEnUno: TodoEnUnoComp,
+  modeloAccesoConfigurador: AccesoConfigurador,
   cardCobertura: CoberturaModel,
   modelTvBeneficios: TVBeneficiosProductos,
   cardContainer: GenericProductCard,
