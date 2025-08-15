a18be3cbb9a99c39feb82de1261e19a47aee7630
juanarodriguez-deloitte <juanarodriguez@deloitte.com>
2025-08-15 17:25:57 -0500

    [Feat]: Maquetacion resumen mobile y ajuste en desktop

--- src/components/molecules/configurador/resumenPaquetes.tsx
+++ src/components/molecules/configurador/resumenPaquetes.tsx
@@ -1,26 +1,88 @@
+import { ComponentsFields } from "@/types/ConfiguradorTypes";
 import { useContent } from "@/utils/ConfiguradorProvider"
 
 export default function ResumenPaquetes() {
 
     const content = useContent();
 
-    const paquetes = content.userAnswers;
+    const paquetes = content.userAnswers as unknown as Record<string, ComponentsFields>;
+    console.log('paquetes', paquetes)
 
-    console.log('resumen paquetes', paquetes)
+    const internet = paquetes.internet;
+    const tv = paquetes.tv;
+    const movil = paquetes.movil;
 
     return (
         <>
-            {/* {
-                Object.entries(paquetes).map(([key, value]) => (
+            {
+                (internet && internet !== null && Object.keys(internet).length > 0) &&
+                <div className="flex flex-col gap-[24px] border-b-1 border-b-gray-150 pt-[24px]">
+                    <div className="flex justify-between w-full font-bold leading-[24px] text-lg">
+                        <h5>Internet</h5>
+                        <h5>Precio promocional</h5>
+                    </div>
 
-                    <div className="pt-[24px]">
-                        <div>
+                    <div className="flex-flex-col gap-[8px] pb-[8px] w-[90%] font-normal leading-[24px] text-base text-gray-250">
+                        <p>
+                            {`Internet de ${internet.fields.minCapacityInternet}MB`}
+                        </p>
+                        <p>
+                            {`Durante los 6 primeros meses contarás con ${internet.fields.maxCapacityInternet}`}
+                        </p>
+                    </div>
+                </div>
+            }
 
+            {
+                (tv && tv !== null && Object.keys(tv).length > 0) &&
+                <div className="flex flex-col">
+                    <div className="flex flex-col gap-[24px] border-b-1 border-b-gray-150 pt-[24px]">
+                        <div className="flex justify-between w-full font-bold leading-[24px] text-lg">
+                            <h5>{tv.paquete.fields.title}</h5>
+                            <h5>{`$${tv.paquete.fields.discountPrice ? tv.paquete.fields.discountPrice : tv.paquete.fields.price}`}</h5>
+                        </div>
+
+                        <div className="flex-flex-col gap-[8px] pb-[8px] w-[90%] font-normal leading-[24px] text-base text-gray-250">
+                            <p>
+                                {`Incluye ${tv.paquete.fields.subTitle}`}
+                            </p>
+                            <p>
+                                {"Entrega a domicilio"}
+                            </p>
+                            <p>
+                                {"Contrato a 12 meses"}
+                            </p>
                         </div>
                     </div>
-                )
-            } */}
-            PAQUETES
+
+                    <div className="py-[24px] border-b-1 border-b-gray-150">
+                        <div className="flex justify-between items-center w-full font-bold leading-[24px] text-lg">
+                            <h5>{"OTTS"}</h5>
+                            <h5>$XXXX</h5>
+                        </div>
+                    </div>
+                </div>
+            }
+
+            {
+                (movil && movil !== null && Object.keys(movil).length > 0) &&
+                <div className="flex flex-col gap-[24px] border-b-1 border-b-gray-150 pt-[24px]">
+                    <div className="flex justify-between w-full font-bold leading-[24px] text-lg">
+                        <h5>{`Línea móvil ${movil.fields.title}`}</h5>
+                        <h5>{`$${tv.fields.discountPrice ? tv.fields.discountPrice : tv.fields.price}`}</h5>
+                    </div>
+
+                    <div className="flex-flex-col gap-[8px] pb-[8px] w-[90%] font-normal leading-[24px] text-base text-gray-250">
+                        <p>
+                            {"Sin portabilidad"}
+                        </p>
+                        <p>
+                            {"Entrega a domicilio"}
+                        </p>
+                    </div>
+                </div>
+            }
+
         </>
     )
 }
--- src/components/molecules/configurador/resumenPedido.tsx
+++ src/components/molecules/configurador/resumenPedido.tsx
@@ -1,71 +1,158 @@
 'use client'
 
-import ButtonGhost from "@/components/atoms/ButtonGhost";
 import { ResumenData } from "@/types/ConfiguradorTypes";
 import { useContent } from "@/utils/ConfiguradorProvider"
-import ResumenPaquetes from "./resumenPaquetes";
+import ResumenContent from "./resumenContent";
+import { Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader, useDisclosure } from "@heroui/react";
+import ButtonGhost from "@/components/atoms/ButtonGhost";
+import { useEffect, useState } from "react";
+
+export const ArrowUpIcon = (props: any) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" >
+            <path d="M19 15L12 9L5 15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export const ArrowDownIcon = (props: any) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M19 9L12 15L5 9" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
 
 export default function ResumenPedido() {
 
     const content = useContent();
-    const resumenCopys = content.dataResumen as ResumenData;
+    const userAnswers = content.userAnswers;
+    const resumenCopys = content.copysResumen as ResumenData;
 
-    // console.log('Respuestas', content.userAnswers)
+    
+
+    useEffect(() => {
+        content.setCheckedPromotions(false)
+    }, [userAnswers])
+
+    const newSelection = (userAnswers && userAnswers !== null && Object.keys(userAnswers).length > 0)
+
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    function CheckPromotions() {
+        content.setCheckedPromotions(true);
+        onOpen()
+    }
+
 
     return (
         <div className="md:border md:rounded-md md:border-gray-150 w-full px-[16px] pt-[24px] pb-[32px] bg-gray-50 md:bg-white-0">
-            <div className="block md:hidden">
-                <h1>{resumenCopys.total.titulo}</h1>
-            </div>
 
-            <div className="hidden md:block">
-                <h1 className="font-bold leading-[24px] text-xl">{resumenCopys.titulo}</h1>
+            {!newSelection ?
+                <button
+                    className="py-[14px] px-[16px] bg-black-0 border-black-0 rounded-md w-full h-[48px] text-white-0 font-semibold leading-[24px] text-lg text-center disabled:bg-gray-150 disabled:text-gray-50"
+                    disabled
+                >
+                    {resumenCopys.boton.comprobarPromociones}
+                </button>
+                :
 
-                <div className="pt-[24px]">
-                    <ResumenPaquetes />
-                </div>
+                <>
+                    <div className="block md:hidden">
+                        <div className="flex flex-col w-full">
+                            <div className="flex justify-between mb-[16px]">
+                                <div className="flex flex-col gap-[8px]">
+                                    <div className="flex gap-[4px] font-normal text-base leading-[24px] text-gray-500 items-baseline">
+                                        <h3 className="font-extrabold text-[32px] leading-[32px] text-black-0">$XXXX</h3>
+                                        <h5>/mes</h5>
+                                        <p>|</p>
+                                        <h5 className="font-bold">Internet</h5>
+                                    </div>
+                                    <div className="font-bold">¡Combina para conseguir ahorros!</div>
+                                </div>
+                                <button
+                                    className="w-[40px] h-[40px] rounded-full border-2 border-black-0 flex items-center justify-center"
+                                    onClick={onOpen}
+                                >
+                                    <ArrowUpIcon />
+                                </button>
+                            </div>
+                            {!content.checkedPromotions ?
+                                <button
+                                    className="py-[14px] px-[16px] bg-black-0 border-black-0 rounded-md w-full h-[48px] text-white-0 font-semibold leading-[24px] text-lg text-center disabled:bg-gray-150 disabled:text-gray-50"
+                                    onClick={() => CheckPromotions()}
+                                >
+                                    {resumenCopys.boton.comprobarPromociones}
+                                </button>
+                                :
+                                <ButtonGhost
+                                    classStyles={"py-[14px] px-[16px] bg-black-0 border-black-0 rounded-md w-full h-[48px] text-white-0 font-semibold leading-[24px] text-lg text-center"}
+                                    text={resumenCopys.boton.contratar.titulo}
+                                    href={resumenCopys.boton.contratar.url}
+                                />
+                            }
+                        </div>
+                    </div>
+                    <Drawer
+                        isOpen={isOpen}
+                        onOpenChange={onOpenChange}
+                        size="full"
+                        placement="bottom"
+                        hideCloseButton
+                        classNames={{
+                            header: "px-[16px] py-[24px]",
+                            body: "px-[16px] py-0 gap-0",
+                            footer: "w-full px-[16px] pt-[32px]"
+                        }}
+                    >
+                        <DrawerContent>
+                            {(onClose) => (
+                                <>
+                                    <DrawerHeader
+                                        className="flex flex-row justify-between items-center"
+                                    >
+                                        <h3 className="font-bold text-xl leading-[24px] text-[#11181C]">{resumenCopys.titulo}</h3>
+                                        <button
+                                            className="w-[40px] h-[40px] rounded-full border-2 border-black-0 flex items-center justify-center"
+                                            onClick={onClose}
+                                        >
+                                            <ArrowDownIcon />
+                                        </button>
+                                    </DrawerHeader>
 
-                <div className="py-[12px] border-b-1 border-b-gray-150">
-                    <div className="flex justify-between items-center w-full font-normal leading-[24px] text-lg">
-                        <h5>{resumenCopys.total.sinDescuentos}</h5>
-                        <h5 className="font-bold">$XXXX</h5>
-                    </div>
-                </div>
+                                    <DrawerBody>
+                                        <ResumenContent />
+                                    </DrawerBody>
 
-                <div className="flex flex-col gap-[24px] py-[12px] border-b-1 border-b-gray-150">
-                    <div className="flex justify-between w-full font-normal leading-[24px] text-lg">
-                        <h5>{resumenCopys.ahorro.paquete}</h5>
-                        <h5>-$XXXX</h5>
-                    </div>
-                    <div className="flex justify-between w-full font-normal leading-[24px] text-lg">
-                        <h5>{resumenCopys.ahorro.domicilio}</h5>
-                        <h5>-$XXXX</h5>
-                    </div>
-                    <div className="flex justify-between w-full font-normal leading-[24px] text-lg">
-                        <h5>{resumenCopys.ahorro.pagoAnticipado}</h5>
-                        <h5>-$XXXX</h5>
-                    </div>
-                </div>
+                                    <DrawerFooter>
+                                        <div className="flex flex-col w-full">
+                                            {!content.checkedPromotions ?
+                                                <button
+                                                    className="py-[14px] px-[16px] bg-black-0 border-black-0 rounded-md w-full h-[48px] text-white-0 font-semibold leading-[24px] text-lg text-center disabled:bg-gray-150 disabled:text-gray-50"
+                                                    onClick={() => CheckPromotions()}
+                                                >
+                                                    {resumenCopys.boton.comprobarPromociones}
+                                                </button>
+                                                :
+                                                <ButtonGhost
+                                                    classStyles={"py-[14px] px-[16px] bg-black-0 border-black-0 rounded-md w-full h-[48px] text-white-0 font-semibold leading-[24px] text-lg"}
+                                                    text={resumenCopys.boton.contratar.titulo}
+                                                    href={resumenCopys.boton.contratar.url}
+                                                />
+                                            }
+                                        </div>
 
-                <div className="py-[12px] border-b-1 border-b-gray-150">
-                    <div className="flex justify-between items-center w-full font-normal leading-[24px] text-lg">
-                        <h5>{resumenCopys.pagoPosterior}</h5>
-                        <h5 className="font-bold">-$XXXX</h5>
-                    </div>
-                </div>
+                                    </DrawerFooter>
+                                </>
+                            )}
+                        </DrawerContent>
+                    </Drawer>
 
-                <div className="flex flex-col gap-[24px]">
-                    <div className="flex justify-between w-full font-bold leading-[40px] text-[32px] pt-[24px]">
-                        <h2>{resumenCopys.total.titulo}</h2>
-                        <h2>$XXXX</h2>
+                    <div className="hidden md:block">
+                        <ResumenContent />
                     </div>
-                    <ButtonGhost
-                        classStyles={"py-[14px] px-[16px] h-[48px] bg-black-0 border-black-0 rounded-md w-full text-white-0 font-semibold leading-[24px] text-lg"}
-                        text={resumenCopys.boton.contratar.titulo}
-                        href={resumenCopys.boton.contratar.url}
-                    />
-                </div>
-            </div>
+                </>
+            }
         </div>
     )
 }
