976a1f66bd1ee0b81b6b0e7dcc1cfce9d4a4519c
MACF94 <migue9048@gmail.com>
2025-09-18 13:51:48 -0500

    feat: Add CheckoutSteps constants for checkout process steps - checkout

---
+++ src/constants/CheckoutSteps.ts
@@ -0,0 +1,8 @@
+export const CheckoutSteps = {
+    STEP1: 'Paquete seleccionado',
+    STEP2: 'Datos personales',
+    STEP3: 'Documentos del titular',
+    STEP4: 'Verificación de contacto',
+    STEP5: 'Fecha y hora de instalación',
+    STEP6: 'Pago',
+}
