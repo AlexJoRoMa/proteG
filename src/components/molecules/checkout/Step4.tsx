92ab5634e79fb344f7d3e1d4be61adb9cba307a7
diolvera <diolvera@deloittemx.com>
2025-11-02 16:28:24 -0600

    disable es-lint validation

--- src/components/molecules/checkout/Step4.tsx
+++ src/components/molecules/checkout/Step4.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useRef, useState } from 'react';
 import { Form, Input } from '@heroui/react'
 import { DeleteIcon, UploadICon } from '@/constants/IconsConstants';
