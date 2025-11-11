36ee03c7859e2ba9786eabe0e37c8a53da5173bd
JorgeAlexis <joromeromartinez@deloittemx.com>
2025-11-11 12:49:59 -0600

    [UPDATE] se agrego logica para hacer mas grandes los assets

--- src/components/molecules/CardTVInternetMovilComponent.tsx
+++ src/components/molecules/CardTVInternetMovilComponent.tsx
@@ -17,9 +17,9 @@ const CardTVInternetMovilComponent = ({card}:CardPropType) => {
 
     const assetsLength =  Array.isArray(card?.fields?.adds) ? card?.fields?.adds.length : 0;
 
-    const gridType =  assetsLength > 4 ? 'grid-cols-4 grid-rows-2 gap-x-4' : 'grid-cols-2 grid-rows-2 gap-x-2'
+    const gridType =  assetsLength > 4 ? 'grid-cols-4 grid-rows-2 gap-x-4' : 'grid-cols-2 grid-rows-2'
 
-    console.log('🦄 assetAdd ')
+
 
   return (
     <div className='px-[16px] md:px-[24px] py-[32px] bg-white 3xl:h-[660px] 2xl:h-[690px] md:h-[660px] xsm:h-[670px] rounded-md relative flex-col flex'>
