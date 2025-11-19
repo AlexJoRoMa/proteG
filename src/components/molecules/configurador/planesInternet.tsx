5544d031a3eaa7cf6f67fda658857bf9c6107486
diolvera <diolvera@deloittemx.com>
2025-11-19 13:31:55 -0600

    FIX: se realizan ajustes reportados en pruebas QA

--- src/components/molecules/configurador/planesInternet.tsx
+++ src/components/molecules/configurador/planesInternet.tsx
@@ -133,7 +133,8 @@ export default function PlanesInternet({ step }: StepProps) {
                                     }}>
                                     <CardHeader>
                                         <div className="flex flex-col text-start">
-                                            <p className="text-base font-normal leading-[27px]">{`${offersCopys.internet.cards.preVelocidad} ${card.velocidadMinima} ${offersCopys.internet.cards.posVelocidad}`}</p>
+                                            
+                                            <p className={`text-base font-normal leading-[27px] ${card.velocidadMinima === card.velocidadMaxima ? 'invisible' : ''}`}>{`${offersCopys.internet.cards.preVelocidad} ${card.velocidadMinima} ${offersCopys.internet.cards.posVelocidad}`}</p>
                                             <p className="leading-[27px] font-extrabold text-2xl">{`${card.velocidadMaxima} ${offersCopys.internet.cards.unidadVelocidad}`}</p>
                                         </div>
                                     </CardHeader>
