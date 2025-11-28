acffba48a214190196c96bc4f5a04b2d8fb80d10
Juan Rodriguez <juanarodriguez@deloitte.com>
2025-11-28 18:18:43 -0500

    [Fix]: validaciones curp/pasaporte

--- src/utils/inputFilters.ts
+++ src/utils/inputFilters.ts
@@ -1,3 +1,5 @@
+import { CURP_REGEX, PASSPORT_REGEX } from "@/constants/InputsConstants";
+
 export function InputFilter(e: React.FormEvent<HTMLInputElement>, type: 'letras' | 'numeros' | 'alfanumerico') {
     const input = e.currentTarget;
     const value = input.value;
@@ -27,3 +29,15 @@ export function InputFilter(e: React.FormEvent<HTMLInputElement>, type: 'letras'
         input.setSelectionRange(start, start);
     }
 }
+
+export function validatePassport(value: string) {
+    const clean = value.replace(/[^a-zA-Z0-9]/g, '').toUpperCase().slice(0, 13);
+    const isValid = PASSPORT_REGEX.test(clean);
+    return { clean, isValid };
+};
+
+export function validateCurp(value: string) {
+    const clean = value.replace(/[^a-zA-Z0-9]/g, '').toUpperCase().slice(0, 18);
+    const isValid = CURP_REGEX.test(clean);
+    return { clean, isValid };
+};
