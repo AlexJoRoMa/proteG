4ee70147a3d7452ef961665f039e070056bd61df
Juan Rodriguez <juanarodriguez@deloitte.com>
2025-12-09 16:50:44 -0500

    [Fix]: ajuste total resumen de compra

--- src/components/molecules/resumenCompra/resumenContent.tsx
+++ src/components/molecules/resumenCompra/resumenContent.tsx
@@ -97,9 +97,8 @@ export default function ResumenContent({ copys, userSelection }: ResumenContentP
 
     const promotions = promoData?.promos;
     const promoVisible = promotions?.filter((promo) => promo.visible === true);
-    const izziAhorro = promoData?.promoPackage?.find(promo => promo.name.toLowerCase().includes('izzi ahorro'));
     const pagoAnticipado = promoData?.promos?.find(promo => promo.promoName.toLowerCase().includes('pago anticipado'));
-    const totalAfterPromos = (Math.abs(Number(izziAhorro?.amount)) || 0) + Math.abs(Number((pagoAnticipado?.promoPrice || 0)));
+    const totalAfterPromos = Math.abs(Number((pagoAnticipado?.promoPrice || 0)));
     const descuentoInternet = pagoAnticipado ? 0 : Math.abs((Number(userSelection?.internet?.paquete?.descuentoPaquete)));
     const descuentoTv = Math.abs((Number(userSelection?.tv?.paquete?.precioPaquete)) - (Number(userSelection?.tv?.paquete?.precioTachado)));
 
@@ -109,8 +108,8 @@ export default function ResumenContent({ copys, userSelection }: ResumenContentP
         + Number(userSelection?.movil?.paquete?.precioPaquete || 0)
         + Number(userSelection?.tv?.paquete?.precioTachado || userSelection?.tv?.paquete?.precioPaquete || 0)
         + (totalOttPrice || 0);
-    const precioTotal = totalSinDescuento && ahorroCombinado ? totalSinDescuento - ahorroCombinado - (Number(izziAhorro?.amount) || 0) - (Math.abs(Number(totalPromoPrice)) || 0) - (Number(descuentoInternet) || 0) : Number(globalIzziSelection?.precioPaquete) + Number(globalIzziSelection?.extras?.precioPaquete || 0) + (totalOttPrice ? totalOttPrice : 0);
-    const ahorroTotal = (Number(descuentoInternet || 0) + Number(izziAhorro?.amount) || 0) + (Number(ahorroCombinado) || 0) + (Number(pagoAnticipado) || 0) + (Number(Math.abs(totalPromoPrice as number)) || 0);
+    const precioTotal = totalSinDescuento && promoData.promos ? totalSinDescuento - ahorroCombinado - (Math.abs(Number(totalPromoPrice)) || 0) - (Number(descuentoInternet) || 0) : totalSinDescuento;
+    const ahorroTotal = (Number(descuentoInternet || 0) + (Number(ahorroCombinado) || 0) + (Number(pagoAnticipado) || 0)) + (Number(Math.abs(totalPromoPrice as number)) || 0);
 
     const promoMeses = calcularPromos(promotions);
     const descuentoMeses = obtenerArray(promoMeses);
@@ -139,12 +138,6 @@ export default function ResumenContent({ copys, userSelection }: ResumenContentP
                         <h1 className="font-bold text-base leading-[24px] mb-[24px]">{resumenCopys.ahorro.titulo}</h1>
 
                         <div className="w-full font-normal leading-[24px] text-lg space-y-2">
-                            {izziAhorro && (
-                                <div className="flex justify-between w-full">
-                                    <h5 className="text-left">{resumenCopys.ahorro.izziAhorro}</h5>
-                                    <h5 className="text-right">-{FormatCurrency(izziAhorro.amount)}</h5>
-                                </div>
-                            )}
                             {userSelection.internet && !pagoAnticipado && (
                                 <div className="flex justify-between w-full">
                                     <h5 className="text-left">{resumenCopys.ahorro.internet}</h5>
@@ -194,7 +187,7 @@ export default function ResumenContent({ copys, userSelection }: ResumenContentP
                             <>
                                 {
                                     descuentoMeses.map((item) => {
-                                        const precioDespues = totalSinDescuento - Math.abs(Number(izziAhorro?.amount)) - Number(ahorroCombinado) + item.totalPromo;
+                                        const precioDespues = totalSinDescuento - Number(ahorroCombinado) + item.totalPromo;
                                         const copyMes = resumenCopys.ahorro.meses[item.mesId];
 
                                         if (precioDespues === precioTotal) {
