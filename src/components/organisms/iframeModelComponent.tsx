209b738e9ac24cd53b625ed4bc503c987fa10073
JorgeAlexis <joromeromartinez@deloittemx.com>
2025-12-11 12:55:39 -0600

    [UPDATE] se agrego dynamic, types, component para iframe en codigo y contentful

---
+++ src/components/organisms/iframeModelComponent.tsx
@@ -0,0 +1,45 @@
+import { IframeModelID } from '@/types/IframeModelTypes';
+import { contentfulClient } from "@/services/contentful/client";
+
+import { Entry, EntrySkeletonType } from "contentful";
+
+
+const IframeModel = async ({id}: IframeModelID) => {
+    
+    
+    const callComponents:Entry<EntrySkeletonType, undefined, string>[] | null = await contentfulClient.getEntries({
+        content_type: "media",
+        'sys.id': id,
+        select: ['fields.iframeUrl'],
+        include: 2,
+    }).then((entriesResponse) => {
+        return entriesResponse.items
+    });
+    
+    if(!callComponents){
+        return null;
+    }
+
+    const entry = callComponents[0];
+    const getIframeURL = entry.fields.iframeUrl as string;
+
+    return(
+        <div className='iframeDiv w-full 
+        4xl:h-[820px] 
+        2xl:h-[850px] xl:h-[870px]
+        md:h-[1030px]
+        sm:h-[1070px] xsm:h-[500px]
+         '>
+        <iframe
+        src={getIframeURL}
+        title='iframe'
+        className='w-full h-full'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+        />
+        </div>
+    )
+
+}
+
+export default IframeModel;
