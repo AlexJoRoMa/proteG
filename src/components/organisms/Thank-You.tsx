3f79027ad07b0ba76629ae932deb3f940001a7e4
Mario <netrix4@gmail.com>
2026-04-29 09:56:49 -0700

    feat: implement user data persistance

--- src/components/organisms/Thank-You.tsx
+++ src/components/organisms/Thank-You.tsx
@@ -1,18 +1,20 @@
 'use client'
 
+import { DropIcon } from "@/constants/IconsConstants";
+import { Accordion, AccordionItem, Card, CardBody } from "@heroui/react";
 import { useThankYou } from "../providers/ThankYouProvider";
 import Image from "next/image";
 import { ThankyouCopys } from "@/types/ThankyouTypes";
 import { FormatCurrency } from "@/utils/Currency";
 import { useEffect, useState } from "react";
 import ButtonGhost from "../atoms/ButtonGhost";
+import ResumenContent from "../molecules/resumenCompra/resumenContent";
 import { useIzziContent } from "@/components/providers/IzziProvider";
+import { ResumenData } from "@/types/ResumenCompra";
 import { redirect } from "next/navigation";
 import izziDataLayerHelpers from "@/utils/izzi-data-layer-helpers";
 import { EVENTS, CURRENCY } from "@/lib/tracking/constants";
 import { pushToDataLayer } from "@/utils/gtm";
-import ResumenDesktop from "../molecules/thankYou/resumenDesktop";
-import ResumenMobile from "../molecules/thankYou/resumenMobile";
 
 type Shift = {
     day: number | string,
@@ -24,13 +26,22 @@ type Shift = {
 
 export default function ThankYou() {
 
-    const { globalDatosContratacion, globalIzziSelection, globalProcessStatus, clearCheckoutFlow, globalFlagDomicilio, totalSinDescuento, precioTotal, coberturaData } = useIzziContent();
-    const { icon, copys} = useThankYou();
+    const { globalUserAnswers, globalDatosContratacion, globalIzziSelection, globalProcessStatus, clearCheckoutFlow, globalFlagDomicilio, totalSinDescuento, precioTotal, coberturaData } = useIzziContent();
+    const { icon, copys, copyResumen } = useThankYou();
 
     const [selectedShift, setSelectedShift] = useState<Shift | null>(null);
     const [purchaseTracked, setPurchaseTracked] = useState(false);
 
     const copy = copys as ThankyouCopys;
+    const resumenCopys = copyResumen as ResumenData;
+
+    useEffect(() => {
+        localStorage.removeItem('PersistentPersonalData')
+        localStorage.removeItem('PersistentDireccionData')
+        localStorage.removeItem('PersistentBillingData')
+        localStorage.removeItem('PersistentAdditionalAddressData')
+
+    }, [])
 
     useEffect(() => {
         if (!globalProcessStatus.accountNumber || !globalDatosContratacion.Pago?.metodoPago) {
@@ -152,14 +163,18 @@ export default function ThankYou() {
         redirect(`${copy.boton.url}`);
     }
 
+    const itemClasses = {
+        indicator: "data-[open=true]:rotate-180",
+        title: "leading-[24px] font-normal text-base",
+        trigger: "pb-[24px]"
+    }
+
     return (
         <section className="mx-[var(--spacing-sm)] 4xl:mx-[var(--spacing-xl)] 3xl:mx-[var(--spacing-lg)] 2xl:mx-[var(--spacing-md)] sm:mx-[var(--spacing-sm)] mb-[16px] xl:mb-[232px]">
             <div className="flex flex-col gap-[12px] text-center mt-[28px]">
                 <h2 className="font-bold text-2xl xl:text-[32px]">{copy.titulo}</h2>
                 <p className="font-normal text-sm xl:text-base">{copy.subtitulo}</p>
             </div>
-
-            {/* Banner */}
 
             <div className="flex flex-row gap-[16px] w-full py-[24px] xl:py-[12px] px-[16px] rounded-md bg-gray-450 mt-[24px] xl:mt-[27px]">
                 {icon && (
@@ -178,7 +193,7 @@ export default function ThankYou() {
                     <span className="font-semibold text-xl xl:text-2xl">{globalIzziSelection?.tituloTriplePlay ? globalIzziSelection.tituloTriplePlay : globalIzziSelection?.titulo}</span>
                     <span className="font-semibold text-xl xl:text-2xl">{`${FormatCurrency(Number(totalSinDescuento))} ${copy.banner.currency}`}</span>
                     {/* <span className="font-semibold text-xl xl:text-2xl">{`${FormatCurrency(Number(globalIzziSelection?.precioPaquete))} ${copy.banner.currency}`}</span> */}
-
+                    
                     {
                         globalFlagDomicilio ?
                             <span className="font-semibold text-base xl:text-lg">{copy.banner.domiciliacion}</span> :
@@ -187,29 +202,46 @@ export default function ThankYou() {
                 </p>
             </div>
 
-            {/* Thank You - Content */}
-
             <div className="grid grid-cols-1 xl:grid-cols-2 mt-[24px] xl:mt-[32px] gap-[40px] xl:gap-[14px]">
-
-                {/* Resumen Pedido */}
-
                 <div className="xl:pr-[13px] border-b-1 xl:border-b-0 xl:border-r-1 border-gray-150">
+                    <div className="block xl:hidden">
+                        <Accordion
+                            showDivider={false}
+                            isCompact
+                            itemClasses={itemClasses}
+                        >
+                            <AccordionItem
+                                key="1"
+                                aria-label="Accordion 1"
+                                title={copy.resumen}
+                                indicator={<DropIcon />}
+                            >
 
-                    {/* Mobile */}
+                                <Card
+                                    key="1"
+                                    classNames={{
+                                        base: "relative flex flex-row gap-[8px] rounded-md shadow-none h-full w-full items-center",
+                                        body: "w-auto py-[17px] px-0",
+                                    }}
+                                >
+                                    <CardBody>
+                                        <div>
+                                            <ResumenContent copys={resumenCopys} userSelection={globalUserAnswers} />
+                                        </div>
+                                    </CardBody>
+                                </Card>
+                            </AccordionItem>
+                        </Accordion >
 
-                    <section className="block xl:hidden">
-                        <ResumenMobile />
-                    </section>
+                    </div>
 
-                    {/* Desktop */}
+                    <div className="hidden xl:block">
+                        <h1 className="mb-[24px] font-normal text-base xl:text-lg">{copy.resumen}</h1>
+                        <ResumenContent copys={resumenCopys} userSelection={globalUserAnswers} />
 
-                    <section className="hidden xl:block">
-                        <ResumenDesktop />
-                    </section>
+                    </div>
 
                 </div>
-
-                {/* Informacion Pedido - Call to Action */}
 
                 <div className="flex flex-col justify-between h-full">
                     <div className="flex-flex-col justify-between">
@@ -242,7 +274,7 @@ export default function ThankYou() {
                                 </p>
                             </div>
 
-                            <div className="text-sm xl:text-base">
+                            <div className="text-base">
                                 <h3 className="font-bold">{copy.info.mensaje.texto1}</h3>
                                 <p>{copy.info.mensaje.texto2}</p>
                             </div>
