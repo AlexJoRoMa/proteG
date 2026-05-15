cf9937f923709755a02e4e5c752d3d7e8d6ee853
Juan Andrés Rodriguez Benavides <juanarodriguez@deloitte.com>
2026-05-15 15:09:25 -0500

    [Feat]: resumen con altura dinamica

--- src/components/molecules/checkout/resumenContainer.tsx
+++ src/components/molecules/checkout/resumenContainer.tsx
@@ -779,12 +779,14 @@ export default function ResumenContainer({ variant }: ResumenContainerProps) {
                         </ResumenMobile>
                     </div>
                 ) : (
-                    <div className="border rounded-md border-gray-150 w-full pt-[24px] bg-white-0 max-h-[414px] flex flex-col">
-                        <ResumenDesktop
-                            resumenCopys={resumenCopys}
-                        >
-                            {renderContinueButton}
-                        </ResumenDesktop>
+                    <div className="sticky top-[120px] my-[24px]">
+                        <div className="border rounded-md border-gray-150 w-full pt-[24px] bg-white-0 max-h-[calc(100dvh-144px)] flex flex-col">
+                            <ResumenDesktop
+                                resumenCopys={resumenCopys}
+                            >
+                                {renderContinueButton}
+                            </ResumenDesktop>
+                        </div>
                     </div>
                 )
             }
--- src/components/organisms/configurador.tsx
+++ src/components/organisms/configurador.tsx
@@ -186,11 +186,14 @@ export default async function Configurador() {
 
                         {/* Resumen desktop*/}
                         <div className="hidden xl:block sticky top-[120px] mb-[24px]">
-                            <div className="border rounded-md border-gray-150 w-full pt-[24px] bg-white-0 max-h-[414px] flex flex-col">
-                                <ResumenContainerDesktop />
-                            </div>
-                            <div className="mx-0 mt-[24px]">
-                                <ResumenInfo />
+                            <div className="flex flex-col gap-[24px] h-[calc(100dvh-144px)]">
+                                <div className="border rounded-md border-gray-150 w-full pt-[24px] bg-white-0 min-h-0 max-h-full flex flex-col">
+                                    <ResumenContainerDesktop />
+                                </div>
+                                <div className="shrink-0">
+                                    <ResumenInfo />
+                                </div>
+
                             </div>
                         </div>
 
